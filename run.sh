dfbcfe09aa1bfe21791038202061492e36f3775f
brichards64 <benjamin.richards@warwick.ac.uk>
2023-07-14 12:23:13 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for file in `find ../ | grep -e '\.cpp' -e '\.hpp' -e '\.c' -e '\.h'`
 do
      
-    clang-format -i --style='file:./hk-ClangFormat/clang-format_HK_15.0.7' $file
+    clang-format -i --style='file:clang-format_HK_15.0.7' $file
         
 done
 
