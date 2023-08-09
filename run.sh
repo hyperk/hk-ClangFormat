fc116e6369574f71d04a3f74cc0a13667d294c33
Tom Dealtry <tom.deal135@gmail.com>
2023-08-09 11:38:06 +0100

    Update run.sh
    Does it make sense to add all possible C++ file extensions to this? It'll mean that all C++ code will be at least clang formatted, even if the file extension goes against the HK style convention

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in `find ../ | grep -e '\.cpp' -e '\.hpp' -e '\.c' -e '\.h'`
+for file in `find ../ | grep -e '\.cpp$' -e '\.hpp$' -e '\.c$' -e '\.h$' -e '\.cc$' -e '\.hh$' -e '\.cxx$' -e '\.hxx$'`
 do
      
     clang-format -i --style='file:clang-format_HK_15.0.7' $file
